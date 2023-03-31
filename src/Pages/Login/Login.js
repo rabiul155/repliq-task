87c9854caa9be18d5f651caccb0a7fb55d7cb0a2
rabiul155 <siyamict@gmail.com>
2023-03-31 16:44:01 +0600

    log in done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 
 
-const SignUp = () => {
+const Login = () => {
 
     const { register, handleSubmit, } = useForm();
 
@@ -15,23 +16,12 @@ const SignUp = () => {
     return (
         <div className=' flex justify-center '>
 
-
-            <form className=' ' onSubmit={handleSubmit(onSubmit)} >
-
+            <form className='' onSubmit={handleSubmit(onSubmit)} >
 
                 <div className="card my-6 flex-shrink-0 w-full shadow-2xl bg-base-100">
-
-                    <div className="card-body w-96 lg:w-[430px]  py-2 ">
-                        <h2 className=' font-bold text-4xl text-blue-700 text-center mb-5'>SignUp</h2>
+                    <div className="w-96 lg:w-[400px] card-body py-2">
+                        <h2 className=' font-bold text-4xl text-blue-700 text-center mb-1'>Login</h2>
                         <hr />
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input
-                                {...register("name")}
-                                type="text" placeholder="Name" required className=" input input-bordered" />
-                        </div>
 
                         <div className="form-control">
                             <label className="label">
@@ -42,14 +32,6 @@ const SignUp = () => {
                                 type="email" placeholder="Email" required className="input input-bordered" />
                         </div>
 
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Image</span>
-                            </label>
-                            <input required
-                                {...register("picture")}
-                                type="file" className="file-input file-input-bordered w-full " />
-                        </div>
 
                         <div className="form-control">
                             <label className="label">
@@ -59,21 +41,30 @@ const SignUp = () => {
                                 {...register("password")}
                                 required type="password" placeholder="password" className="input input-bordered" />
                         </div>
-                        <div className="form-control mt-5">
-                            <button type='submit' className="btn btn-primary">signup</button>
+                        <div className="flex items-center justify-between mt-2">
+                            <div className="flex items-center">
+                                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
+                            </div>
+
+                            <div className="text-sm">
+                                <Link className="font-medium text-indigo-600 hover:text-indigo-500">Forgot password?</Link>
+                            </div>
+                        </div>
+                        <div className="form-control mt-2">
+                            <button type='submit' className="btn btn-primary">Login</button>
                         </div>
                         <div className="divider">or</div>
                         <div className="form-control ">
-                            <button type='submit' className="btn btn-outline mb-4">signup with google</button>
+                            <button type='submit' className="btn btn-outline mb-4">Login with google</button>
                         </div>
+
                     </div>
                 </div>
             </form>
-
-
 
         </div>
     );
 };
 
-export default SignUp;
+export default Login;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([{
@@ -9,7 +10,11 @@ export const router = createBrowserRouter([{
     children: [
         {
             path: '/signup',
-            element: <SignUp> </SignUp>
+            element: <SignUp> </SignUp>,
+        },
+        {
+            path: '/login',
+            element: <Login></Login>
         }
     ]
 }])
